cd0615d1e3f7db71e9792ad8a39d491ec64f3e57
LuminousV <farelreyhan6@gmail.com>
2025-05-07 08:05:52 +0700

    Creating the project setup & folder structure

---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const config = require("./webpack.config");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(config, {
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true,
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    assetModuleFilename: "img/[name][ext]",
+    clean: true,
+  },
+});
