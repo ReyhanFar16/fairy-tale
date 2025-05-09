c2fb6beca8980f0ddaca1127f96a2a3f97f9ddad
LuminousV <farelreyhan6@gmail.com>
2025-05-09 14:03:53 +0700

    dev: configure webpack and adding the template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 // This is entry point
+
+import css from "./assets/styles/style.css";
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,36 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   devtool: false,
+  module: {
+    rules: [
+      //* ------- Babel Loader ------- */
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            targets: "defaults",
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      //* ------- File Loader ------- */
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
+  //* ------- Plugins ------- */
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/templates/index.html",
+    }),
+  ],
 };
